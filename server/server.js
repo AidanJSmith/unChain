1d7c28344498971980efe15e6e24279cae7cd27a
Aidan Smith <lolbot55555@gmail.com>
2020-04-25 17:39:40 -0700

    Ostensibly Functional GPS Distance Stuff

--- server/server.js
+++ server/server.js
@@ -3,6 +3,24 @@ var app = express();
 var fs = require("fs");
 app.use(bodyParser.json());
 
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) { //https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
+   var R = 6371; // Radius of the earth in km
+   var dLat = deg2rad(lat2-lat1);  // deg2rad below
+   var dLon = deg2rad(lon2-lon1); 
+   var a = 
+     Math.sin(dLat/2) * Math.sin(dLat/2) +
+     Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+     Math.sin(dLon/2) * Math.sin(dLon/2)
+     ; 
+   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+   var d = R * c; // Distance in km
+   return d;
+ }
+ 
+ function deg2rad(deg) {
+   return deg * (Math.PI/180)
+ }
+
 //Get User Data
 app.get('/getUser/:id', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
@@ -250,11 +268,46 @@ app.get('/marketProducts/:marketid', function (req, res) {
 
    })
 })
-//Get Nearest Farmers' Market to user
 
-//Add Subcontractor
+//Get Nearest Farmers' Market to user
+app.get('/marketsNear/:lat/:long', function (req, res) {
+   let latitude=req.params.lat;
+   let longitude=req.params.long;
+   fs.readFile( __dirname + "/" + "markets.json", 'utf8', function (err, data) {
+      let withinrange=[]
+      for (let market of Object.keys(JSON.parse(data))) {
+         let distance= getDistanceFromLatLonInKm(latitude,longitude,data[market]["gps"].split("lat")[1].split("long")[0],data[market]["gps"].split("lat")[1].split("long")[1])
+         if (distance<=25) {
+            withinrange.push([market,distance]);
+         }
+      }
+      res.end(JSON.stringify(withinrange));
+   });
+})
 
 //Find Subcontractors On Farmer's Route
+app.get('/subcontractNear/:lat/:long', function (req, res) {
+   let latitude=req.params.lat;
+   let longitude=req.params.long;
+   fs.readFile( __dirname + "/" + "farmers.json", 'utf8', function (err, data) {  // Account for if they're within tolerance of a market or a farmer's farm.
+      let withinrange=[] 
+      for (let farmer of Object.keys(JSON.parse(data))) {
+         let distance= getDistanceFromLatLonInKm(latitude,longitude,data[farmer]["gps"].split("lat")[1].split("long")[0],data[farmer]["gps"].split("lat")[1].split("long")[1])
+         if (distance<=25) {
+            withinrange.push([market,distance]);
+         }
+      }
+      fs.readFile( __dirname + "/" + "markets.json", 'utf8', function (err, data) { // Account for if they're within tolerance of a market or a farmer's farm.
+         for (let market of Object.keys(JSON.parse(data))) {
+            let distance= getDistanceFromLatLonInKm(latitude,longitude,data[market]["gps"].split("lat")[1].split("long")[0],data[market]["gps"].split("lat")[1].split("long")[1])
+            if (distance<=25) {
+               withinrange.push([market,distance]);
+            }
+         }
+      })
+      res.end(JSON.stringify(withinrange));
+   })
+})
 
 
 var server = app.listen(8081, function () {
